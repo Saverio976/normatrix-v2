a9699bb3db865d68e5c128f5bea9e22020bdd0c7
Xavier Mitault <xavier.mitault@epitech.eu>
2022-10-01 12:15:49 +0200

    Rewrite how regex return

--- normatrix/regexs/r_declaration.py
+++ normatrix/regexs/r_declaration.py
@@ -1,32 +1,27 @@
 import sys
 
 import regex
+from regexs import regexs_class
 
-# https://regex101.com/r/fBLUOp/1
+regex.cache_all(True)
+
+# https://regex101.com/r/fBLUOp/2
 re = r"\w{1,}( \*{0,} {0,}\w{1,}(\[[0-9]{0,}\]){0,}){1,} "
-r"{0,}= {0,}(([0-9]{1,})|(\".*\")|(\{.*\})|([&*]{1,}\w{1,})|(NULL)) {0,};{0,}"
+r"{0,}= {0,}(([0-9]{1,})|(\".*\")|(\{.*\})|([&*]{1,}\w{1,})|(NULL)) {0,};{1}"
 reg = regex.compile(re)
 
 
 def match(text: str, timeout=1):
     try:
-        return reg.match(text, timeout=timeout)
+        res = reg.match(text, timeout=timeout)
     except TimeoutError as esc:
         sys.stderr.write(f"ERROR: {__file__}:match: {esc}: {text}")
         return None
+    return regexs_class.RegexsResult(text, res.start(), res.end())
 
 
-def sub(text: str, replace: str, timeout=1):
+def sub(text: str, replace: str, timeout=1) -> None:
     try:
         return reg.sub(replace, text, timeout=timeout)
     except TimeoutError as esc:
         sys.stderr.write(f"ERROR: {__file__}:sub: {esc}: {text}")
-        return None
-
-
-def search(text: str, timeout=1):
-    try:
-        return reg.search(text, timeout=timeout)
-    except TimeoutError as esc:
-        sys.stderr.write(f"ERROR: {__file__}:search: {esc}: {text}")
-        return None
---
+++ normatrix/regexs/regexs_class.py
@@ -0,0 +1,22 @@
+class RegexsResult:
+    def __init__(self, text: str, start: int, end: int) -> None:
+        if len(text) <= end:
+            raise ValueError(
+                f"{__file__}: ERROR: end of matching is out of range ({end})"
+            )
+        if len(text) <= start:
+            raise ValueError(
+                f"{__file__}: ERROR: start of matching is out of range ({start})"
+            )
+        if end < 0:
+            raise ValueError(
+                f"{__file__}: ERROR: end of matching is out of range ({end})"
+            )
+        if start < 0:
+            raise ValueError(
+                f"{__file__}: ERROR: end of matching is out of range ({start})"
+            )
+        self.text = text
+        self.start = start
+        self.end = end
+        self.matching = text[start : end + 1]
