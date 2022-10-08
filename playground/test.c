02bd1bc756f81c390e0f7e5b88e7b64f678e57a6
Xavier Mitault <xavier.mitault@epitech.eu>
2022-10-08 13:03:47 +0200

    Add playground c test file for compilation test

---
+++ playground/test.c
@@ -0,0 +1,46 @@
+int abc(int
+a, char c,
+const long
+int de, char *
+const uh, char ret[
+5
+]/*, char ret
+[5]*/) {
+    return 1;
+}
+
+int abc(int
+a, char c,
+const long
+int de, char *
+const uh, char ret[
+5
+]/*, char ret
+[5]*/)
+;
+
+int main(){
+    char ret[
+        5
+    ] = {0};
+    char hh
+        [5] = {5}
+    ;
+    char *
+        const uh =
+        (char *) 5;
+    const
+        long
+        int
+        de = 2
+    ;
+    char c = 5
+    ;
+    int
+    a
+    =
+    2
+    ;
+    abc(4, c, de, uh, ret);
+    return 0;
+}
