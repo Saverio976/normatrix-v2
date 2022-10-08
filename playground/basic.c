50a1134fc96f10d8988b109893d35d1d6928b1a7
Xavier Mitault <xavier.mitault@epitech.eu>
2022-10-08 21:37:40 +0200

    Add test file

---
+++ playground/basic.c
@@ -0,0 +1,6 @@
+int main(int ac, char **argv)
+{
+    int a = 5;
+
+    printf("coucou %d", a);
+}
---
+++ playground/while.c
@@ -0,0 +1,12 @@
+int main(void)
+{
+    int a = 5;
+
+    while (a) {a--;}
+    while (a != 5) {
+        while (a > 0) {
+            a++;
+        }
+    }
+    return 0;
+}
