98d5e0bd993c64c9010ed6c805787a75266cfd63
Xavier Mitault <xavier.mitault@epitech.eu>
2022-10-08 13:02:28 +0200

    Update regexs

--- normatrix/regexs/r_declaration.py
+++ normatrix/regexs/r_declaration.py
@@ -6,9 +6,10 @@ from regexs import regexs_class
 
 regex.cache_all(True)
 
-# https://regex101.com/r/fBLUOp/2
-re = r"\w{1,}( \*{0,} {0,}\w{1,}(\[[0-9]{0,}\]){0,}){1,} "
-r"{0,}= {0,}(([0-9]{1,})|(\".*\")|(\{.*\})|([&*]{1,}\w{1,})|(NULL)) {0,};{1}"
+# https://regex101.com/r/fBLUOp/2 (old)
+# re = r"\w{1,}( \*{0,} {0,}\w{1,}(\[[0-9]{0,}\]){0,}){1,} {0,}= {0,}(([0-9]{1,})|(\".*\")|(\{.*\})|([&*]{1,}\w{1,})|(NULL)) {0,};{1}" # noqa: E501
+# https://regex101.com/r/ohzzuZ/1 (new)
+re = r"\w{1,} {0,}\n{0,}( \*{0,} {0,}\n{0,} {0,}\w{1,} {0,}\n{0,} {0,}(\[ {0,}\n{0,} {0,}[0-9]{0,} {0,}\n{0,} {0,}\]){0,} {0,}\n{0,} {0,}){1,} {0,}= {0,}\n{0,} {0,}(([0-9]{1,})|((\".*\" {0,}\\){0,}(\".*\"){1})|(\{.*\})|([&*]{1,} {0,}\n{0,} {0,}\w{1,})|(NULL)|(\(.{2,}\).*)) {0,}\n{0,} {0,};{1}"  # noqa: E501
 reg = regex.compile(re)
 
 
@@ -25,4 +26,4 @@ def sub(text: str, replace: str, timeout=1) -> None:
     try:
         return reg.sub(replace, text, timeout=timeout)
     except TimeoutError as esc:
-        sys.stderr.write(f"ERROR: {__file__}:sub: {esc}: {text}")
+        print(f"ERROR: {__file__}:sub: {esc}: {text}", file=sys.stderr)
