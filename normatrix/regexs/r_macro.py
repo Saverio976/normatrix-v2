cf5380aef7df63985e0615a83b732ea5367902b6
Xavier Mitault <xavier.mitault@epitech.eu>
2022-10-08 13:05:05 +0200

    Add macro and prototype regex

---
+++ normatrix/regexs/r_macro.py
@@ -0,0 +1,25 @@
+import sys
+from typing import Union
+
+import regex
+from regexs import regexs_class
+
+# https://regex101.com/r/tp3bAf/1
+re = r"#define \w{1,}(\((\w,{0,} {0,}){0,}\)){0,1} {0,}(.*\\\n){0,}(.*){1}"
+reg = regex.compile(re)
+
+
+def match(text: str, timeout=1) -> Union[None, regexs_class.RegexsResult]:
+    try:
+        res = reg.match(text, timeout=timeout)
+    except TimeoutError as esc:
+        print(f"ERROR: {__file__}:match: {esc}: {text}", file=sys.stderr)
+        return None
+    return regexs_class.RegexsResult(text, res.start(), res.end())
+
+
+def sub(text: str, replace: str, timeout=1) -> None:
+    try:
+        return reg.sub(replace, text, timeout=timeout)
+    except TimeoutError as esc:
+        print(f"ERROR: {__file__}:sub: {esc}: {text}", file=sys.stderr)
---
+++ normatrix/regexs/r_prototype.py
@@ -0,0 +1,27 @@
+import sys
+from typing import Union
+
+import regex
+from regexs import regexs_class
+
+regex.cache_all(True)
+
+# https://regex101.com/r/DJ3cg4/1
+re = r"\w{1,}( \*{0,} {0,}\w{1,}(\[[0-9]{0,}\]){0,}){1,} {0,}\((\w{1,} {0,}\n{0,}( \*{0,} {0,}\n{0,} {0,}\w{1,}(\[[0-9]{0,}\]){0,}){1,} {0,}\n{0,} {0,},{0,1} {0,}\n{0,} {0,}){0,}\) {0,}\n{0,};{1}"  # noqa: E501
+reg = regex.compile(re)
+
+
+def match(text: str, timeout=1) -> Union[None, regexs_class.RegexsResult]:
+    try:
+        res = reg.match(text, timeout=timeout)
+    except TimeoutError as esc:
+        print(f"ERROR: {__file__}:match: {esc}: {text}", file=sys.stderr)
+        return None
+    return regexs_class.RegexsResult(text, res.start(), res.end())
+
+
+def sub(text: str, replace: str, timeout=1) -> None:
+    try:
+        return reg.sub(replace, text, timeout=timeout)
+    except TimeoutError as esc:
+        print(f"ERROR: {__file__}:sub: {esc}: {text}", file=sys.stderr)
