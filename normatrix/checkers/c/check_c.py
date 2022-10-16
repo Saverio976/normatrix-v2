3b4ac5b2bae28fe04da8cc8f286725d51c37deb3
Xavier Mitault <xavier.mitault@epitech.eu>
2022-10-16 15:47:04 +0200

    Add checkers

--- normatrix/checkers/c/check_c.py
+++ normatrix/checkers/c/check_c.py
@@ -10,7 +10,13 @@ from normatrix.checkers.c.nb_line_per_func import check_nlpf
 from normatrix.checkers.c.nb_params import check_np
 from normatrix.checkers.c.nested_branch import check_nb
 from normatrix.checkers.c.operator import check_op
+from normatrix.checkers.c.parenthesis import check_parenthesis
+from normatrix.checkers.c.preprocessor import check_preprocessor
+from normatrix.checkers.c.snake_case import check_sc
+from normatrix.checkers.c.statements import check_statements
+from normatrix.checkers.c.subscriptor import check_subscriptor
 from normatrix.checkers.c.tabulation import check_tab
+from normatrix.checkers.c.two_space import check_ts
 from normatrix.errors.norm import _TemplateNormError
 from normatrix.parser.cfile import CFile
 
@@ -26,6 +32,12 @@ checkerss = [
     check_np,
     check_nb,
     check_op,
+    check_parenthesis,
+    check_preprocessor,
+    check_sc,
+    check_statements,
+    check_subscriptor,
+    check_ts,
 ]
 
 
