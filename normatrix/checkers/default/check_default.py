d54266c594fb0ae746926f8de40eba7f23d8d281
Xavier Mitault <xavier.mitault@epitech.eu>
2022-10-31 09:52:49 +0100

    Change projectname

--- normatrix/checkers/default/check_default.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-from normatrix.errors.norm import _TemplateNormError
-from normatrix.parser.file import DefaultFile
-
-
-def check(file: DefaultFile) -> List[_TemplateNormError]:
-    return []
--- normatrix/checkers/makefile/check_makefile.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-from normatrix.errors.norm import _TemplateNormError
-from normatrix.parser.makefile import MakeFile
-
-
-def check(file: MakeFile) -> List[_TemplateNormError]:
-    return []
