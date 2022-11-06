153f770b8662807c22d58b64bec15fffaaf0c807
Xavier Mitault <xavier.mitault@epitech.eu>
2022-11-06 19:46:49 +0100

    Add check if there is a gitignore

--- norma2/config/from_gitignore.py
+++ norma2/config/from_gitignore.py
@@ -9,9 +9,11 @@ from norma2.config.config_class import Config
 def from_gitignore(console: Console, conf_path: str = ".") -> Config:
     full_path = os.path.join(conf_path, ".gitignore")
     full_path = os.path.abspath(full_path)
+    conf = Config(console)
+    if not os.path.isfile(full_path):
+        return conf
     parser = IgnoreParser()
     parser.parse_rule_file(full_path)
     matches = parser.match
-    conf = Config(console)
     conf.gitignore_matches = matches
     return conf
